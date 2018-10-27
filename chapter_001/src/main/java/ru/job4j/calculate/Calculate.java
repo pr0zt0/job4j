762618297f0f3645326ded9ba7b2e6704f268efb
pr0zt0 <MKGumenyuk@yandex.ru>
2018-10-27 17:42:32 +0300

    1.4 JUnit [#78638]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -13,7 +13,18 @@ public class Calculate{
 	*	@param args - arg.
 	*	
 	*/
-	public static void main(String[] args){
-System.out.println("Hello World");
-}
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+	
+	/**
+	*	Method echo.
+	*	@param name Your name
+	*	@return Echo plus your name
+	*/
+	public String echo(String name)
+	{
+		return "Echo, echo, echo: " + name;
+	}
 }
