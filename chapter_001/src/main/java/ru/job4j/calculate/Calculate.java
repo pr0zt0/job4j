bde44c487c17f95cc06dee5791eef5a076f62dcc
pr0zt0 <MKGumenyuk@yandex.ru>
2018-10-27 15:13:20 +0300

    1.3. JavDoc. Code convetion. [78637]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+*	Класс Calculate выводит 
+*	сообщение Hello World
+*	
+*	@author MKG
+*	@version 1.0
+*/
 public class Calculate{
+	/**
+	*	Main
+	*	@param args - arg.
+	*	
+	*/
 	public static void main(String[] args){
 System.out.println("Hello World");
 }
