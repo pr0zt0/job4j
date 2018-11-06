d7b0263c3d1e085961bc12187e7b37b65c868415
pr0zt0 <MKGumenyuk@yandex.ru>
2018-11-07 00:38:22 +0300

    6.7. [#78618]. Изменил mono.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,16 +3,12 @@ package ru.job4j.array;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        int i, j;
-         for (i = 1, j = 1; i < data.length && j < data.length; i++, j++) {
-            if (data[0][0] != data [i][j]) {
+         for (int i = 0; i < data.length; i++) {
+            if (data[0][0] != data [i][i]) {
                 result = false;
                 break;
             }
-        }
-
-        for (i = 0, j = data.length - 1; i < data.length - 1 && j > 0; i++, j--) {
-            if (data[i][j] != data[i + 1][j - 1]) {
+            if (data[0][data.length - 1] != data[i][data.length - 1 - i]) {
                 result = false;
                 break;
             }
