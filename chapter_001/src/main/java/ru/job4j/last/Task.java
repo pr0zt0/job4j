3b7f3c324b115baba10cce28e1cfb7da3cac5cc1
pr0zt0 <MKGumenyuk@yandex.ru>
2018-11-13 00:33:45 +0300

    Вопросы. Убраны комментарии и добавлен тест [#83561]

--- chapter_001/src/main/java/ru/job4j/last/Task.java
+++ chapter_001/src/main/java/ru/job4j/last/Task.java
@@ -1,5 +1,7 @@
 package ru.job4j.last;
 
+import ru.job4j.array.BubbleSort;
+
 import java.util.Arrays;
 
 /**
@@ -9,16 +11,19 @@ import java.util.Arrays;
  * @version 1.0
  */
 public class Task {
-
+    /**
+     * Method shateTwoHeap
+     * @param array - исходный массив
+     * @return array - в котором хранится два числа
+     * array[0] - первая половина
+     * array[1] - вторая половина
+     */
     public int[] shateTwoHeap(int[] array) {
         double summ = 0;
         int heap1 = 0, heap2 = 0;
-
-        //Цикл для подсчета суммы всех чисел в массиве
         for (int i = 0; i < array.length; i++) {
             summ += array[i];
         }
-        //выставляем приблизительную границу
         summ /= 2;
 
         for (int i = 0; i < array.length; i++) {
@@ -30,7 +35,51 @@ public class Task {
         }
         array[0] = heap1;
         array[1] = heap2;
-
         return Arrays.copyOf(array, 2);
+    }
+    /**
+     * Method shateFromMinToMax
+     * @param array - исходный массив
+     * @return array - в котором хранится два числа
+     * array[0] - первая половина
+     * array[1] - вторая половина
+     */
+    public int[] shateFromMinToMax(int[] array) {
+        double summ = 0;
+        int heap1 = 0, heap2 = 0, temp;
+
+        for (int i = 0; i < array.length; i++) {
+            summ += array[i];
+        }
+        summ /= 2;
+        BubbleSort bsort = new BubbleSort();
+        array = bsort.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            if (heap1 < heap2 || heap1 == heap2) {
+                heap1 += array[i];
+            } else {
+                heap2 += array[i];
+            }
+        }
+        array[0] = heap1;
+        array[1] = heap2;
+        return Arrays.copyOf(array, 2);
+    }
+    /**
+     * Method chooseBetter
+     * Выбирает какой метод лучше разбил массив
+     * @param arrayHeap - что получилось на выходе из 1 метода
+     * @param arrayMinToMax - что получилось на выходе из 2 метода
+     * @return arrayHeap - если 1 метод лучше
+     * @return arrayMinToMax - если 2 метод лучше
+     * array[0] - первая половина
+     * array[1] - вторая половина
+     */
+    public int[] chooseBetter(int[] arrayHeap, int[] arrayMinToMax) {
+        if (Math.abs(arrayHeap[0] - arrayHeap[1]) < Math.abs(arrayMinToMax[0] - arrayMinToMax[1])) {
+            return arrayHeap;
+        } else {
+            return arrayMinToMax;
+        }
     }
 }
--- chapter_001/src/test/java/ru/job4j/last/TaskTest.java
+++ chapter_001/src/test/java/ru/job4j/last/TaskTest.java
@@ -6,6 +6,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TaskTest {
+    /**
+     * Test shateTwoHeap
+     */
     @Test
     public void whenFirstArray10z5z1ThenFirstHeap10Second6() {
         Task last = new Task();
@@ -14,7 +17,9 @@ public class TaskTest {
         int[] exp = new int[] {10, 6};
         assertThat(res, is(exp));
     }
-
+    /**
+     * Test shateTwoHeap
+     */
     @Test
     public void whenArray12z24z41z2ThenFirstHeap38Second41() {
         Task last = new Task();
@@ -23,5 +28,41 @@ public class TaskTest {
         int[] exp = new int[] {38, 41};
         assertThat(res, is(exp));
     }
+    /**
+     * Test shateTwoHeap
+     */
+    @Test
+    public void whenArrays1and2and3and4and5ThenFirstHeap6SecondHeap9() {
+        Task last = new Task();
+        int[] primalArray = new int[] {1, 2, 3, 4, 5};
+        int[] res = last.shateFromMinToMax(primalArray);
+        int[] exp = new int[] {9, 6};
+        assertThat(res, is(exp));
+    }
+    /**
+     * Test shateFromMinToMax
+     */
+    @Test
+    public void whenArray12and24and41and2ThenFirstHeap53Second26() {
+        Task last = new Task();
+        int[] primalArray = new int[] {12, 24, 41, 2};
+        int[] res = last.shateFromMinToMax(primalArray);
+        int[] exp = new int[] {53, 26};
+        assertThat(res, is(exp));
+    }
+    /**
+     * Test chooseBetter
+     */
+    @Test
+    public void whenFirstArrayAndSecondAraySameThenBetterFirst() {
+        Task last = new Task();
+        int[] primalArray = new int[] {12, 24, 41, 2};
+        int[] first = last.shateTwoHeap(primalArray);
+        int[] secondArray = new int[] {12, 24, 41, 2};
+        int[] second = last.shateFromMinToMax(secondArray);
+        int[] res = last.chooseBetter(first, second);
+        assertThat(res, is(first));
+    }
 }
+
 
