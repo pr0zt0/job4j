01f513f9c50074f7d31139e6cd8fb29517a9a521
pr0zt0 <MKGumenyuk@yandex.ru>
2019-01-21 00:10:03 +0300

    4. Используя шаблон проектирования - стратегию[#78677]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -10,15 +10,10 @@ import java.io.PrintStream;
 public class PaintTest {
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
         PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
         System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(
                 new String(out.toByteArray()),
                 is(
